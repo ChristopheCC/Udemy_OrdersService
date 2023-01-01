1c2c3ced59d89869f4ca563f961b3b968a33eaab
ChristopheCC <christophe.chane@gmail.com>
2023-01-01 18:41:09 +0800

    Section 17 : Fetch Payment Details

--- src/main/java/com/udemy/estore/OrdersService/saga/OrderSaga.java
+++ src/main/java/com/udemy/estore/OrdersService/saga/OrderSaga.java
@@ -3,12 +3,16 @@ package com.udemy.estore.OrdersService.saga;
 import com.udemy.estore.OrdersService.core.events.OrderCreatedEvent;
 import com.udemy.estore.core.commands.ReserveProductCommand;
 import com.udemy.estore.core.events.ProductReservedEvent;
+import com.udemy.estore.core.model.User;
+import com.udemy.estore.core.query.FetchUserPaymentDetailsQuery;
 import org.axonframework.commandhandling.CommandCallback;
 import org.axonframework.commandhandling.CommandMessage;
 import org.axonframework.commandhandling.CommandResultMessage;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.modelling.saga.SagaEventHandler;
 import org.axonframework.modelling.saga.StartSaga;
+import org.axonframework.queryhandling.QueryGateway;
 import org.axonframework.spring.stereotype.Saga;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +25,9 @@ public class OrderSaga {
 
     @Autowired
     private transient CommandGateway commandGateway;
+
+    @Autowired
+    private transient QueryGateway queryGateway;
 
     @StartSaga
     @SagaEventHandler(associationProperty = "orderId")
@@ -53,5 +60,26 @@ public class OrderSaga {
         // process user payment
         LOGGER.info("ProductReservedEvent is called for orderId : " + productReservedEvent.getOrderId()
                 + " and productId : "+ productReservedEvent.getProductId());
+
+        FetchUserPaymentDetailsQuery fetchUserPaymentDetailsQuery = FetchUserPaymentDetailsQuery.builder()
+                .userId(productReservedEvent.getUserId())
+                .build();
+
+
+        User userPaymentDetails;
+
+        try {
+            userPaymentDetails = queryGateway.query(fetchUserPaymentDetailsQuery,
+                    ResponseTypes.instanceOf(User.class)).join();
+        } catch (Exception ex){
+            LOGGER.error(ex.getMessage());
+            // Start compensating transaction
+            return;
+        }
+        if (userPaymentDetails == null){
+            // Start compensating transaction
+            return;
+        }
+        LOGGER.info("Successfully fetched user payment details for user : "+ userPaymentDetails.getFirstName());
     }
 }
